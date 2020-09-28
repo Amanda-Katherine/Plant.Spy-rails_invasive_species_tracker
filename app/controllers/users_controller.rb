29d0104df344d1e1dcc13464ea38bf3824ae78c9
Amanda_Katherine <walker.akw@gmail.com>
2020-09-28 17:03:27 -0400

    add CRUD functionality skeleton to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,9 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  # def new
+  # end
+
   def create
     @user = User.new(user_params)
 
@@ -11,8 +14,31 @@ class UsersController < ApplicationController
       #redirect to user path OR redirect to species path (need to decide)
     else
       render :new
+      #render error message? 
     end
   end
 
-  
+  def show
+    # @user = find_by(id: #current_user)
+  end
+
+  def edit
+    #@user = user.find_by(id: params[:id])
+  end
+
+  def update
+    if #authenticate params id is current_user
+      @user.update(user_params)
+      #redirect to user_path
+    else
+      #error message
+      #redirect (decide where to redirect to)
+    end
+  end
+
+  def destroy
+    @user.destroy
+    #redirect to homepage/root path? 
+  end
+
 end
