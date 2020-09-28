c6090c63469c223640412daf81434c330df9146d
Amanda_Katherine <walker.akw@gmail.com>
2020-09-28 19:43:57 -0400

    create view directory and folders, add custom routes to route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  root 'welcome#home'
+  
   resources :posts
   resources :invasive_species
   resources :users
 
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
