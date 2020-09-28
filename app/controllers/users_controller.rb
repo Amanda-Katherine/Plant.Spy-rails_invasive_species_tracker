88dbc36cc62fb3411dc22e7c3c48021dbb164cb6
Amanda_Katherine <walker.akw@gmail.com>
2020-09-28 16:43:17 -0400

    create 'create action' in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,17 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      #start session
+      #redirect to user path OR redirect to species path (need to decide)
+    else
+      render :new
+    end
+  end
+
+  
 end
