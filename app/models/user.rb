d9053867c8d1dfb95530318e5d04efd5c95b7e4d
Amanda_Katherine <walker.akw@gmail.com>
2020-09-29 00:37:10 -0400

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
     has_many :posts
     has_many :invasive_species, through: :posts
 
-    validates :name, :username, :about_me, :password, :location, presence: true
+    validates :name, :username, :about_me, :password, :location, presence: true, on: :create
+    validates :username, :password, presence: true#, on: login 
     has_secure_password
 end
 
