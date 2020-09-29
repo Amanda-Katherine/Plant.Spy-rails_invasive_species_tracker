1568acf4bb60117fcc4c4a203128a22546fbe71e
Amanda_Katherine <walker.akw@gmail.com>
2020-09-28 20:17:55 -0400

    create login page form in sessions#new

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
+  # post '/login', to: 'sessions#create'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
