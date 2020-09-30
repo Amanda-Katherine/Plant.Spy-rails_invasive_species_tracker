046b1c1c7a43e39bb428404afa9892f23096a71c
Amanda-Katherine <walker.akw@gmail.com>
2020-09-30 15:46:21 -0400

    make login omniauth buttons iinline on login page, add tester css to first button

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
     has_many :posts
     has_many :invasive_species, through: :posts
 
-    validates :name, :username, :email, presence: true, on: 'users#create'
-    validates :username, :email, presence: true, on: 'sessions#login_via_account'
+    validates :name, :username, :email, presence: true, on: :create
     validates :email, presence: true, on: 'sessions#login_via_omniauth'
-    validates :username, uniqueness: true 
+    validates :username, :email, uniqueness: true 
+
     
     has_secure_password
 end
