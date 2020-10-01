047f9d6cc08f99b8a254046b012a59fe9bf10ef1
Amanda-Katherine <walker.akw@gmail.com>
2020-09-30 20:04:43 -0400

    create Environment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_29_224036) do
+ActiveRecord::Schema.define(version: 2020_10_01_000127) do
+
+  create_table "environments", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "invasive_species", force: :cascade do |t|
     t.string "common_name"
