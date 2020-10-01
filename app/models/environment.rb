7863df2348fe26d32f50f1e304491c3a518ba3c0
Amanda-Katherine <walker.akw@gmail.com>
2020-09-30 21:21:47 -0400

    add associations to Post, Environment, and PostEnvironment classes.

--- app/models/environment.rb
+++ app/models/environment.rb
@@ -1,2 +1,4 @@
 class Environment < ApplicationRecord
+    has_many :post_environments
+    has_many :posts, through: :post_environments
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+    has_many :post_environments
+    has_many :environments, through: :post_environments
     belongs_to :invasive_species
     belongs_to :user
 end
