70baa039fc3a6de6485144c66f251c3de377f01b
Amanda-Katherine <walker.akw@gmail.com>
2020-10-03 13:15:04 -0400

    update suser controller flash message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
+    @user = User.new(user_params)
 
-    if user.save
-      session[:user_id] = user.id
+    if @user.save
+      session[:user_id] = @user.id
       redirect_to invasive_species_path
     else
-      @errors = user.errors.full_messages
+      flash[:danger] = "#{@user.errors.full_messages.to_sentence}"
       render :new
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
     has_many :posts
     has_many :invasive_species, through: :posts
 
-    validates :name, :username, presence: true, on: 'users#create'
-    validates :email, presence: true, on: 'sessions#login_via_omniauth'
-    validates :username, uniqueness: true, on: :create
-    validates :email, uniqueness: true, on: 'sessions#login_via_omniauth'
+    validates :name, :username, presence: true
+    # validates :email, presence: true, on: 'sessions#login_via_omniauth'
+    validates :username, uniqueness: true
+    # validates :email, :username, uniqueness: true, on: 'sessions#login_via_omniauth'
     
     has_secure_password
 end
